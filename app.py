cb1136d68b67dd1a2d32f314777661a1bc8b9072
KennethSettens <kennysettens1234@gmail.com>
2023-08-21 18:50:08 -0400

    new user not being add to database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from models import db, connect_db, User
 
 app = Flask(__name__)
@@ -9,6 +9,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.app_context().push()
 
 connect_db(app)
+db.create_all()
 
 @app.route('/')
 def index():
@@ -21,19 +22,28 @@ def list_users():
     return render_template('list_users.html', users=users)
 
 
-# @app.route('/users/<int:user_id>')
-# def show_user(user_id):
-#     return render_template('user_detail.html', user_id=user_id)
+@app.route('/users/<int:user_id>')
+def show_user(user_id):
+    return render_template('userDetails.html', user_id=user_id)
 
 
-# @app.route('/users/new', methods=['GET'])
-# def new_user():
-#     return render_template('add_user.html')
+@app.route('/users/new', methods=['GET'])
+def new_user():
+    return render_template('newUser.html')
 
 
-# @app.route('/users/new', methods=['POST'])
-# def add_user():
-#     return redirect(url_for('list_users'))
+@app.route('/users/new', methods=['POST'])
+def add_user():
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    image_url = request.form.get('imageurl')
+
+    new_user = User(
+        FirstName=first_name, LastName=last_name, imageurl=image_url)
+
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/users')    
 
 
 # @app.route('/users/<int:user_id>/edit', methods=['GET'])
