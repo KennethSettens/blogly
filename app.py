b4928e3806e4c73452c785641b7d3193df02178e
KennethSettens <kennysettens1234@gmail.com>
2023-08-21 14:52:52 -0400

    database error

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.app_context().push()
 
 connect_db(app)
 
 @app.route('/')
 def index():
-    return redirect(url_for('list_users'))
+    return redirect('/users')
 
 
 @app.route('/users')
@@ -20,33 +21,33 @@ def list_users():
     return render_template('list_users.html', users=users)
 
 
-@app.route('/users/<int:user_id>')
-def show_user(user_id):
-    return render_template('user_detail.html', user_id=user_id)
+# @app.route('/users/<int:user_id>')
+# def show_user(user_id):
+#     return render_template('user_detail.html', user_id=user_id)
 
 
-@app.route('/users/new', methods=['GET'])
-def new_user():
-    return render_template('add_user.html')
+# @app.route('/users/new', methods=['GET'])
+# def new_user():
+#     return render_template('add_user.html')
 
 
-@app.route('/users/new', methods=['POST'])
-def add_user():
-    return redirect(url_for('list_users'))
+# @app.route('/users/new', methods=['POST'])
+# def add_user():
+#     return redirect(url_for('list_users'))
 
 
-@app.route('/users/<int:user_id>/edit', methods=['GET'])
-def edit_user(user_id):
-    return render_template('edit_user.html', user_id=user_id)
+# @app.route('/users/<int:user_id>/edit', methods=['GET'])
+# def edit_user(user_id):
+#     return render_template('edit_user.html', user_id=user_id)
 
 
-@app.route('/users/<int:user_id>/edit', methods=['POST'])
-def update_user(user_id):
-    return redirect(url_for('show_user', user_id=user_id))
+# @app.route('/users/<int:user_id>/edit', methods=['POST'])
+# def update_user(user_id):
+#     return redirect(url_for('show_user', user_id=user_id))
 
 
-@app.route('/users/<int:user_id>/delete', methods=['POST'])
-def delete_user(user_id):
-    return redirect(url_for('list_users'))
+# @app.route('/users/<int:user_id>/delete', methods=['POST'])
+# def delete_user(user_id):
+#     return redirect(url_for('list_users'))
 
-db.create_all()
+
